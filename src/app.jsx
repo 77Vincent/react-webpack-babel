14830b50967647453c0b926f199710add7f66fc0
Vincent Wen <vincent@it-consultis.com>
2017-03-30 14:55:17 +0800

    normal update

--- src/app.jsx
+++ src/app.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
+import 'scaffold.css';
 import '../styles/index.scss';
+import Toggling from 'toggling';
 
 export default class App extends React.Component {
   render() {
@@ -8,7 +10,20 @@ export default class App extends React.Component {
         <h1>It Works!</h1>
         <p>This React project just works including <span className="redBg">module</span> local styles.</p>
         <p>Enjoy!</p>
+        <button className="trigger">Trigger</button>
+        <h1 className="target">Target</h1>
       </div>
     )
+  }
+
+  componentDidMount() {
+    new Toggling({
+      trigger: '.trigger',
+      target: '.target',
+      handler(tar, tri) {
+        this.toggle(tar);
+        this.toggleClass(tri, 'toggle');
+      }
+    })
   }
 }
